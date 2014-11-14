fc490530d82e8fb5558320d03c45985e149ab7b6
harghan <godin.charles62@gmail.com>
2014-11-14 14:17:07 +0100

    Commit Story 3 Test Red

--- test/test.js
+++ test/test.js
@@ -14,3 +14,12 @@ PalettoTestCase.prototype.testStory2 = function () {
     e.initialisation();
     assertEquals(e.selectColor("a6"), "Yellow");
 };
+
+PalettoTestCase.prototype.testStory3 = function () {
+    var e = new Engine();
+    e.initialisation();
+    assertEquals(e.selectColor("a6"), "Yellow");
+    assertEquals(e.p1TakePiece("a6"),"");
+    assertEquals(e.PiecesOnBoard(), 35);
+    assertEquals(e.p1NbPieces(), 1);
+};
