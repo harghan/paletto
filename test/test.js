e99c1172289fa11d7cc9a25a782ab16d946a04e5
harghan <godin.charles62@gmail.com>
2014-11-14 13:35:41 +0100

    Commit Story 1 Test Red

--- test/test.js
+++ test/test.js
@@ -3,4 +3,8 @@
 var PalettoTestCase = TestCase("PalettoTestCase");
 
 PalettoTestCase.prototype.testStory1 = function () {
+    var e = new Engine();
+    e.initialisation();
+    assertEquals(e.juxtapositionOK(), true);
+
 };
